30826262229919222b5f4ba251c1d555b4f90442
Ivy Xing <ivyx99@gmail.com>
2017-05-13 17:59:30 -0700

    Initial Commit

---
+++ LunchGroupGenerator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LunchGroupGenerator
+//
+//  Created by Ivy Xing on 5/13/17.
+//  Copyright © 2017 Ivy Xing. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
